c32efb5e6a84ba0b74a361b95844dfbec0ea3342
omaryazghi98-code <omaryazghi98@gmail.com>
2026-08-26 01:29:45 +0100

    feat(domain): export contract pricing engine

--- packages/domain/src/index.ts
+++ packages/domain/src/index.ts
@@ -5,6 +5,7 @@ export * from './vehicleStateMachine.js';
 export * from './reservation.js';
 export * from './pricing.js';
 export * from './contract.js';
+export * from './contractPricing.js';
 export * from './alerts.js';
 export * from './permissions.js';
 export * from './labels.js';
